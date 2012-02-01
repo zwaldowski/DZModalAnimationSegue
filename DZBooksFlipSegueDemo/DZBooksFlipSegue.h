0196b43b79ca594b58beadf3010add8f56d149e6
Zachary Waldowski <zwaldowski@gmail.com>
2012-01-31 19:50:24 -0500

    Initial commit of segue.
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

---
+++ DZBooksFlipSegueDemo/DZBooksFlipSegue.h
@@ -0,0 +1,13 @@
+//
+//  DZBooksFlipSegue.h
+//  DZBooksFlipSegueDemo
+//
+//  Created by Zachary Waldowski on 1/31/12.
+//  Copyright (c) 2012 Dizzy Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZBooksFlipSegue : UIStoryboardSegue
+
+@end
