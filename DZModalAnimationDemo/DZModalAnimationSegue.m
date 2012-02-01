8c14e484e2fde17e2058f390668198c51610d66c
Zachary Waldowski <zwaldowski@gmail.com>
2012-02-01 14:11:46 -0500

    Abstract out modal presentation for animations that occur in different places.
    Abstract out the modal presentation component for animations that occur in different places relative to when the actual call to presentViewController:animated:completion:.
    
    Signed-off-by: Zachary Waldowski <zwaldowski@gmail.com>

--- DZModalAnimationDemo/DZModalAnimationSegue.m
+++ DZModalAnimationDemo/DZModalAnimationSegue.m
@@ -45,9 +45,9 @@ static SEL selectorForAnimation(DZCustomModalAnimation animation) {
 
 @interface DZModalAnimationSegue()
 
-+ (void)performSelector:(SEL)selector withObject:(id)p1 withObject:(id)p2 withObject:(id)p3;
++ (void)performAnimation:(DZCustomModalAnimation)animation fromController:(id)from toController:(id)to reverse:(BOOL)reverse completion:(void(^)(void))completion;
 
-+ (void)flipToView:(UIView *)toView fromView:(UIView *)fromView completion:(void(^)(void))completion;
++ (void)flipFrom:(UIViewController *)fromController to:(UIViewController *)toController reverse:(BOOL)reverse completion:(void(^)(void))completion;
 
 @end
 
@@ -55,7 +55,13 @@ static SEL selectorForAnimation(DZCustomModalAnimation animation) {
 
 @synthesize animation, animationStatusBarStyle;
 
-+ (void)performSelector:(SEL)selector withObject:(id)p1 withObject:(id)p2 withObject:(id)p3 {
++ (void)performAnimation:(DZCustomModalAnimation)animation fromController:(id)from toController:(id)to reverse:(BOOL)reverse completion:(void(^)(void))completion {
+	SEL selector;
+	switch (animation) {
+		case DZCustomModalAnimationBooksFlip:	selector = @selector(flipFrom:to:reverse:completion:); break;
+		default:								selector = NULL; break;
+	}
+	
 	NSMethodSignature *sig = [self methodSignatureForSelector:selector];
 	if (!sig)
 		return;
@@ -63,94 +69,106 @@ static SEL selectorForAnimation(DZCustomModalAnimation animation) {
 	NSInvocation* invo = [NSInvocation invocationWithMethodSignature:sig];
 	[invo setTarget:self];
 	[invo setSelector:selector];
-	[invo setArgument:&p1 atIndex:2];
-	[invo setArgument:&p2 atIndex:3];
-	[invo setArgument:&p3 atIndex:4];
+	[invo setArgument:&from atIndex:2];
+	[invo setArgument:&to atIndex:3];
+	[invo setArgument:&reverse atIndex:4];
+	[invo setArgument:&completion atIndex:5];
 	[invo invoke];
 }
 
-+ (void)flipToView:(UIView *)toView fromView:(UIView *)fromView completion:(void(^)(void))completion {
-	CALayer *contentLayer = [CALayer layer];
-	contentLayer.frame = [toView bounds];
-	contentLayer.backgroundColor = [UIColor blackColor].CGColor;
++ (void)flipFrom:(UIViewController *)fromController to:(UIViewController *)toController reverse:(BOOL)reverse completion:(void(^)(void))completion {
+	UIView *fromView = fromController.view;
+	UIView *toView = toController.view;
 	
-	CATransformLayer *transformLayer = [CATransformLayer layer];
-	transformLayer.frame = contentLayer.bounds;
-	CATransform3D t = CATransform3DIdentity;
-	t.m34 = -1.0 / 850.0;
-	transformLayer.sublayerTransform = t;
+	void(^animation)(void) = ^{
+		CALayer *contentLayer = [CALayer layer];
+		contentLayer.frame = [toView bounds];
+		contentLayer.backgroundColor = [UIColor blackColor].CGColor;
+		
+		CATransformLayer *transformLayer = [CATransformLayer layer];
+		transformLayer.frame = contentLayer.bounds;
+		CATransform3D t = CATransform3DIdentity;
+		t.m34 = -1.0 / 850.0;
+		transformLayer.sublayerTransform = t;
+		
+		CGFloat animationDepth = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? -80.0f : -40.0f;
+		
+		CALayer *oldLayer = [CALayer layer];
+		CALayer *newLayer = [CALayer layer];
+		CALayer *rightLayer = [CALayer layer];
+		
+		oldLayer.contents = (__bridge id)imageForView(fromView);
+		newLayer.contents = (__bridge id)imageForView(toView);
+		rightLayer.contents = (__bridge id)[UIImage imageNamed:@"DZBooksFlipSegueSide"].CGImage;
+		
+		oldLayer.frame = newLayer.frame = transformLayer.bounds;
+		oldLayer.zPosition = newLayer.zPosition = rightLayer.zPosition = 
+		oldLayer.anchorPointZ = newLayer.anchorPointZ = animationDepth;
+		
+		rightLayer.frame = CGRectMake(CGRectGetMidX(transformLayer.bounds)+animationDepth, 0, -2*animationDepth, transformLayer.bounds.size.height);
+		rightLayer.anchorPointZ = CGRectGetMidX(transformLayer.bounds);
+		
+		[[toView layer] addSublayer:contentLayer];
+		[contentLayer addSublayer:transformLayer];
+		[transformLayer addSublayer:oldLayer];
+		[transformLayer addSublayer:newLayer];
+		[transformLayer addSublayer:rightLayer];
+		
+		CAKeyframeAnimation *zAnim     = [CAKeyframeAnimation animationWithKeyPath:@"zPosition"];
+		CAKeyframeAnimation *frontFlip = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+		CAKeyframeAnimation *backFlip  = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+		CAKeyframeAnimation *rightFlip = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+		
+		zAnim.keyTimes = frontFlip.keyTimes =
+		backFlip.keyTimes = rightFlip.keyTimes = [NSArray arrayWithObjects:
+												  [NSNumber numberWithDouble:0.0],
+												  [NSNumber numberWithDouble:0.5],
+												  [NSNumber numberWithDouble:1.0], nil];
+		
+		zAnim.values = [NSArray arrayWithObjects:
+						[NSNumber numberWithDouble:animationDepth],
+						[NSNumber numberWithDouble:4.75*animationDepth],
+						[NSNumber numberWithDouble:animationDepth], nil];
+		frontFlip.values = [NSArray arrayWithObjects:
+							degreeTransform(0),
+							degreeTransform(-90),
+							degreeTransform(-180), nil];
+		backFlip.values = [NSArray arrayWithObjects:
+						   degreeTransform(180),
+						   degreeTransform(90),
+						   degreeTransform(0), nil];
+		rightFlip.values = [NSArray arrayWithObjects:
+							degreeTransform(-90),
+							degreeTransform(-180),
+							degreeTransform(90), nil];
+		
+		frontFlip.fillMode = backFlip.fillMode = rightFlip.fillMode = kCAFillModeBoth;
+		frontFlip.removedOnCompletion = backFlip.removedOnCompletion = rightFlip.removedOnCompletion = NO;
+		
+		CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+		[CATransaction commitWithDuration:1.0 timingFunction:timingFunction transactions:^{
+			[oldLayer addAnimation:zAnim forKey:@"OldFlipZ"];
+			[newLayer addAnimation:zAnim forKey:@"NewFlipZ"];
+			[rightLayer addAnimation:zAnim forKey:@"RightFlipZ"];
+			[oldLayer addAnimation:frontFlip forKey:@"OldFlip"];
+			[newLayer addAnimation:backFlip forKey:@"NewFlip"];
+			[rightLayer addAnimation:rightFlip forKey:@"RightFlip"];
+			[[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+			[[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+		} completion:^{
+			[contentLayer removeFromSuperlayer];
+			[[UIApplication sharedApplication] performSelector:@selector(endIgnoringInteractionEvents) withObject:nil afterDelay:0.2];
+			[[UIDevice currentDevice] performSelector:@selector(beginGeneratingDeviceOrientationNotifications) withObject:nil afterDelay:0.5];
+			if (completion)
+				completion();
+		}];
+	};
 	
-	CGFloat animationDepth = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) ? -80.0f : -40.0f;
-
-	CALayer *oldLayer = [CALayer layer];
- 	CALayer *newLayer = [CALayer layer];
-	CALayer *rightLayer = [CALayer layer];
-	
-    oldLayer.contents = (__bridge id)imageForView(fromView);
-    newLayer.contents = (__bridge id)imageForView(toView);
-	rightLayer.contents = (__bridge id)[UIImage imageNamed:@"DZBooksFlipSegueSide"].CGImage;
-
-	oldLayer.frame = newLayer.frame = transformLayer.bounds;
-	oldLayer.zPosition = newLayer.zPosition = rightLayer.zPosition = 
-	oldLayer.anchorPointZ = newLayer.anchorPointZ = animationDepth;
-	
-	rightLayer.frame = CGRectMake(CGRectGetMidX(transformLayer.bounds)+animationDepth, 0, -2*animationDepth, transformLayer.bounds.size.height);
-	rightLayer.anchorPointZ = CGRectGetMidX(transformLayer.bounds);
-    
-	[[toView layer] addSublayer:contentLayer];
-	[contentLayer addSublayer:transformLayer];
-	[transformLayer addSublayer:oldLayer];
-	[transformLayer addSublayer:newLayer];
-	[transformLayer addSublayer:rightLayer];
-    
-    CAKeyframeAnimation *zAnim     = [CAKeyframeAnimation animationWithKeyPath:@"zPosition"];
-    CAKeyframeAnimation *frontFlip = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
-    CAKeyframeAnimation *backFlip  = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
-	CAKeyframeAnimation *rightFlip = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
-	
-	zAnim.keyTimes = frontFlip.keyTimes =
-	backFlip.keyTimes = rightFlip.keyTimes = [NSArray arrayWithObjects:
-							   [NSNumber numberWithDouble:0.0],
-							   [NSNumber numberWithDouble:0.5],
-							   [NSNumber numberWithDouble:1.0], nil];
-	
-	zAnim.values = [NSArray arrayWithObjects:
-					[NSNumber numberWithDouble:animationDepth],
-					[NSNumber numberWithDouble:4.75*animationDepth],
-					[NSNumber numberWithDouble:animationDepth], nil];
-    frontFlip.values = [NSArray arrayWithObjects:
-						degreeTransform(0),
-						degreeTransform(-90),
-						degreeTransform(-180), nil];
-    backFlip.values = [NSArray arrayWithObjects:
-					   degreeTransform(180),
-					   degreeTransform(90),
-					   degreeTransform(0), nil];
-    rightFlip.values = [NSArray arrayWithObjects:
-					   degreeTransform(-90),
-					   degreeTransform(-180),
-					   degreeTransform(90), nil];
-	
-	frontFlip.fillMode = backFlip.fillMode = rightFlip.fillMode = kCAFillModeBoth;
-	frontFlip.removedOnCompletion = backFlip.removedOnCompletion = rightFlip.removedOnCompletion = NO;
-	
-	CAMediaTimingFunction *timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-	[CATransaction commitWithDuration:1.0 timingFunction:timingFunction transactions:^{
-		[oldLayer addAnimation:zAnim forKey:@"OldFlipZ"];
-		[newLayer addAnimation:zAnim forKey:@"NewFlipZ"];
-		[rightLayer addAnimation:zAnim forKey:@"RightFlipZ"];
-		[oldLayer addAnimation:frontFlip forKey:@"OldFlip"];
-		[newLayer addAnimation:backFlip forKey:@"NewFlip"];
-		[rightLayer addAnimation:rightFlip forKey:@"RightFlip"];
-		[[UIApplication sharedApplication] beginIgnoringInteractionEvents];
-		[[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
-	} completion:^{
-		[contentLayer removeFromSuperlayer];
-		[[UIApplication sharedApplication] performSelector:@selector(endIgnoringInteractionEvents) withObject:nil afterDelay:0.2];
-		[[UIDevice currentDevice] performSelector:@selector(beginGeneratingDeviceOrientationNotifications) withObject:nil afterDelay:0.5];
-		if (completion)
-			completion();
-	}];
+	if (reverse) {
+		[fromController dismissViewControllerAnimated:NO completion:animation];
+	} else {
+		[fromController presentViewController:toController animated:NO completion:animation];
+	}
 }
 
 - (void)perform {
@@ -158,15 +176,10 @@ static SEL selectorForAnimation(DZCustomModalAnimation animation) {
 	UIStatusBarStyle oldStatusBarStyle = [[UIApplication sharedApplication] statusBarStyle];
 	
 	[self.destinationViewController setModalPresentationStyle:UIModalPresentationFullScreen];
+	[self.destinationViewController setCustomAnimation:self.animation];
 	[[UIApplication sharedApplication] setStatusBarStyle:self.animationStatusBarStyle animated:YES];
 	
-	[self.sourceViewController presentViewController:self.destinationViewController animated:NO completion:^{
-		SEL animationSelector = selectorForAnimation(self.animation);
-		[self.destinationViewController setCustomAnimation:self.animation];
-		UIView *toView = [self.destinationViewController view];
-		UIView *fromView = [self.sourceViewController view];
-		[[self class] performSelector:animationSelector withObject:toView withObject:fromView withObject:NULL];
-		
+	[DZModalAnimationSegue performAnimation:self.animation fromController:self.sourceViewController toController:self.destinationViewController reverse:NO completion:^{
 		[self.destinationViewController setModalPresentationStyle:oldPresentationStyle];
 		[[UIApplication sharedApplication] setStatusBarStyle:oldStatusBarStyle animated:YES];
 	}];
@@ -182,9 +195,16 @@ static char DZCustomAnimationStatusBarKey;
 - (void)dismissViewControllerWithCustomAnimation:(void (^)(void))completion {
 	UIViewController *fromViewController = self.presentedViewController ?: self;
 	UIViewController *toViewController = self.presentingViewController ?: self;
-	SEL animationSelector = selectorForAnimation(fromViewController.customAnimation);
-	[fromViewController dismissViewControllerAnimated:NO completion:^{
-		[DZModalAnimationSegue performSelector:animationSelector withObject:toViewController.view withObject:fromViewController.view withObject:completion];
+	
+	UIModalPresentationStyle oldPresentationStyle = [fromViewController modalPresentationStyle];
+	UIStatusBarStyle oldStatusBarStyle = [[UIApplication sharedApplication] statusBarStyle];
+	
+	[fromViewController setModalPresentationStyle:UIModalPresentationFullScreen];
+	[[UIApplication sharedApplication] setStatusBarStyle:self.animationStatusBarStyle animated:YES];
+
+	[DZModalAnimationSegue performAnimation:fromViewController.customAnimation fromController:fromViewController toController:toViewController reverse:YES completion:^{
+		[fromViewController setModalPresentationStyle:oldPresentationStyle];
+		[[UIApplication sharedApplication] setStatusBarStyle:oldStatusBarStyle animated:YES];
 	}];
 }
 
